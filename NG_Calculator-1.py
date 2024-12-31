0b764c8aa37956b15813f49ea6358ad0bcb7892f
Boulder <157924017+Derek-Stafford@users.noreply.github.com>
2024-12-31 02:19:41 -0700

    Update NG_Calculator-1.py

--- NG_Calculator-1.py
+++ NG_Calculator-1.py
@@ -11,20 +11,22 @@ D2 = float(input(bold+italic+"D2 = "+end))
 D3 = float(input(bold+italic+"D3 = "+end))
 
 # Pass/Fail Storage
-Fail_Count = 0
+Pass = 0
+Fail = 0
 Results = [] 
 
 # Calculations
-H = (1.25 * D3) + ((D1 + D2) / 2)
+H = (1.25*D3)+((D1+D2)/2)
 print(bold+italic+f"H = {H:.4f}"+end)
 
 # Threshold Messages, Checks, Storage
 def check_value(name, value, threshold):
-  global Fail_Count
+  global Pass, Fail
   if value > threshold:
     Fail_Count += 1
     Results.append(bold+italic + red + f"{name} Failed"+end)
   else:
+    Pass += 1
     Results.append(bold+italic+ green +f"{name} Passed"+end)
 # Check Values against Threshold Conditins                 
 check_value("D1", D1, 0.066)
@@ -33,9 +35,9 @@ check_value("D3", D3, 0.056)
 check_value("H", H, 0.078)
 
 # Summary of Results                  
-if Fail_Count == 0:
+if Pass == 4:
   print(bold+italic+green+"All values passed"+end)
-elif Fail_Count == 4: 
+elif Fail == 4: 
   print(bold+italic+red+"All values failed"+end)
 else:
   for Result in Results:
